a3c6f79fbee35028d4698018d880483f2b18a725
weston <117936288+westonon@users.noreply.github.com>
2023-03-26 13:49:40 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 
 game 'gta5'
 author 'ZAD4Y'
-description 'No NPC'
-version '1.0.0'
+description 'https://github.com/westonon/weston-npc'
+version '1.0.1'
 
 client_script 'main.lua'
